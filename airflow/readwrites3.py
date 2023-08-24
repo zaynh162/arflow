5dcf3f934c5032a00890bf6d2f45ba64c9f162c5
zaynh162 <zaynh162@gmail.com>
2023-08-24 21:22:08 +0530

    First commit1

--- airflow/readwrites3.py
+++ airflow/readwrites3.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     # See PyCharm help at https://www.jetbrains.com/help/pycharm/
     #   spark: SparkSession = SparkSession.builder.config("spark.jars.packages", "com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:3.3.3").master("local[*]").appName(
     #       "SparkByExamples.com").getOrCreate()
-    #
+
     df = spark.read.options(header='True', inferSchema='True').csv("s3://glue-zayn/bootcamp/orders/orders.csv")
     df.write.parquet("s3://zayn-emr/capstone/airflow_out/order_par/")
